252eb09118eef8c44730be024646bbeacf8774ad
ChoiSeongHwan <a01036459946@gmail.com>
2024-06-22 07:21:59 +0900

    ai작업

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -2,20 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
 public class Monster : MonoBehaviour
 {
-    public Transform target;
+    public enum CurrentState { idle, trace, attack, dead};
+    public CurrentState curState = CurrentState.idle;
 
-    NavMeshAgent nmAgent;
-    // Start is called before the first frame update
-    void Start()
+    private Transform _transform;
+    private Transform playerTransform;
+    private NavMeshAgent nvAgent;
+    private Animator _animator;
+
+    public float traceDist = 15.0f;
+    public float attackDist = 3.2f;
+
+    private bool isDead = false;
+
+
+     void Start()
     {
-        nmAgent = GetComponent<NavMeshAgent>();
+        _transform = this.gameObject.GetComponent<Transform>();
+        playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        nvAgent = this.gameObject.GetComponent<NavMeshAgent>();
+        _animator = this.gameObject.GetComponent<Animator>();
+
+        StartCoroutine(this.CheckState());
+        StartCoroutine(this.CheckStateForAction());
     }
 
-    // Update is called once per frame
-    void Update()
+         IEnumerator CheckState()
     {
-        nmAgent.SetDestination(target.position);
+        while (!isDead)
+        {
+            yield return new WaitForSeconds(0.2f);
+
+            float dist = Vector3.Distance(playerTransform.position, _transform.position);
+
+            if (dist <= attackDist)
+            {
+                curState = CurrentState.attack;
+            }
+            else if(dist <= traceDist)
+            {
+                curState = CurrentState.trace;
+            }
+            else
+            {
+                curState = CurrentState.idle;
+            }
+        }
+    }
+
+    
+    IEnumerator CheckStateForAction()
+    {
+        while (!isDead)
+        {
+            switch (curState)
+            {
+                case CurrentState.idle:
+                    nvAgent.Stop();
+                    _animator.SetBool("IsTrace", false);
+                    break;
+
+                case CurrentState.trace:
+                    nvAgent.destination = playerTransform.position;
+                    nvAgent.Resume();
+                    _animator.SetBool("IsTrace", true);
+                    break;
+
+                case CurrentState.attack:
+                    break;
+            }
+            yield return null;
+        }
     }
 }
+  
